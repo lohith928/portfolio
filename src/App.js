f888782171b84f0edebd4603a2144e8f1de3b649
Sivakumar1001 <sivakumarkrishnan1001@gmail.com>
2024-10-15 14:55:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+            <h1>LOHITH  R</h1>
+        </center>
+        <h5>CONTACT INFO</h5>
+        <ul>
+            <li>phone no:8220306203</li>
+            <li>Email id:logith181@gmail.com</li>
+            <li>Address:Kumbakonam,Tamilnadu</li>
+        </ul>
+        <h5>PROFILE SUMMARY</h5>
+        <p>Passionate and driven full-stack developer with a solid foundation in both front-end and back-end technologies. Proficient in HTML, CSS, and JavaScript frameworks like React for dynamic user interfaces, along with back-end expertise in Node.js and Express. Experienced in working with databases such as MongoDB and SQL to design efficient data models. Strong problem-solving skills and a keen eye for detail, dedicated to creating seamless, user-centric applications. Eager to stay current with industry trends and continuously improve coding skills through hands-on projects and collaboration.</p>
+    <h5>EDUCATION</h5>
+    <ul>Bachelor of engineering</ul>
+    <h5>TECHNICAL SKILLS</h5>
+    <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>GITHUB</li>
+    </ul>
+    <h5>CERTIFICATION</h5>
+    <ul>
+        <li>full stack web development</li>
+        <li>artificial intelligence</li>
+    </ul>
+    <h5>EXTRA CURRICULAR ACTIVITIES</h5>
+    <ul>
+        <li>kabaddi</li>
+        <li>volleyball</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
